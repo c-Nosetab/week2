48fa46a6f8222a4e4c25c88996c4f3a1b6c433ef
c-Nosetab <cpbateson@gmail.com>
2017-05-02 20:22:23 -0500

    added scss and gems

--- contacts-app/app/controllers/people_controller.rb
+++ contacts-app/app/controllers/people_controller.rb
@@ -1,9 +1,7 @@
 class PeopleController < ApplicationController
 
 def contact
-  @contact1 = Person.first
-  @contact2 = Person.find(2)
-  @contact3 = Person.last
+  @contact = Person.all
 
   render 'contact_list.html.erb'
 end
